24a5b7812c1d3c442d8255c22e58ca81f0a0e784
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-26 15:31:30 -0500

    Added Table component, fetch call, and sorting methods.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,138 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Table from './Table'
+
+const intialState = [
+    {
+        username: '',
+        img: '',
+        recent: '',
+        alltime: ''
+    }
+]
+
+const upArrow = '↑'
+const downArrow = '↓'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor () {
+        super()
+        this.getData = this.getData.bind(this)
+        this.sortDescendingRecent = this.sortDescendingRecent.bind(this)
+        this.sortAscendingRecent = this.sortAscendingRecent.bind(this)
+        this.sortDescendingAllTime = this.sortDescendingAllTime.bind(this)
+        this.sortAscendingAllTime = this.sortAscendingAllTime.bind(this)
+        this.state = {
+            fetchData: intialState,
+            recent: {
+                sort: false,
+                arrow: '',
+                type: ''
+            },
+            alltime: {
+                sort: false,
+                arrow: '',
+                type: ''
+            }
+        }
+    }
+
+    componentDidMount () {
+        this.getData()
+    }
+
+    getData () {
+        fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+            .then(data => data.json())
+            .then(data => this.setState({fetchData:data}))
+    }
+
+    sortDescendingRecent () {
+        const recent = this.state.fetchData.sort((a,b) => b.recent - a.recent)
+        this.setState({
+            fetchData: recent,
+            recent: {
+                sort: true,
+                arrow: downArrow,
+                type: 'descend'
+            },
+            alltime: {
+                sort: false,
+                arrow: '',
+                type: ''
+            }
+        })
+    }
+
+    sortAscendingRecent () {
+        const recent = this.state.fetchData.sort((a,b) => a.recent - b.recent)
+        this.setState({
+            fetchData: recent,
+            recent: {
+                sort: true,
+                arrow: upArrow,
+                type: 'ascend'
+            },
+            alltime: {
+                sort: false,
+                arrow: '',
+                type: ''
+            }
+        })
+    }
+
+    sortDescendingAllTime () {
+        const alltime = this.state.fetchData.sort((a,b) => b.alltime - a.alltime)
+        this.setState({
+            fetchData: alltime,
+            recent: {
+                sort: false,
+                arrow: '',
+                type: ''
+            },
+            alltime: {
+                sort: true,
+                arrow: downArrow,
+                type: 'descend'
+            }
+        })
+    }
+
+    sortAscendingAllTime () {
+        const alltime = this.state.fetchData.sort((a,b) => a.alltime - b.alltime)
+        this.setState({
+            fetchData: alltime,
+            recent: {
+                sort: false,
+                arrow: '',
+                type: ''
+            },
+            alltime: {
+                sort: true,
+                arrow: upArrow,
+                type: 'ascend'
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>FreeCodeCamp Leaderboard</h2>
+                </div>
+                <Table
+                    data={this.state.fetchData}
+                    recent={this.state.recent}
+                    alltime={this.state.alltime}
+                    descendRecent={this.sortDescendingRecent}
+                    ascentRecent={this.sortAscendingRecent}
+                    descendAllTime={this.sortDescendingAllTime}
+                    ascendAllTime={this.sortAscendingAllTime}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
